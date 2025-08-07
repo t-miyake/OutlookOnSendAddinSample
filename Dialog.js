684f09b3adfba87a5881b2a55e3cee82129a83ab
T.Miyake <t@miyake.me>
2025-08-08 08:51:04 +0900

    Update

--- Dialog.js
+++ Dialog.js
@@ -1,13 +1,13 @@
-Office.onReady = function () { }
+Office.onReady = function () {};
 
 setTimeout(function () {
-    Office.context.ui.messageParent("cancel");
+  Office.context.ui.messageParent("cancel");
 }, 180000);
 
 $("#sendButton").click(function () {
-    Office.context.ui.messageParent("send");
+  Office.context.ui.messageParent("send");
 });
 
 $("#cancelButton").click(function () {
-    Office.context.ui.messageParent("cancel");
+  Office.context.ui.messageParent("cancel");
 });
---
+++ function.js
@@ -0,0 +1,48 @@
+﻿"use strict";
+
+const dialogPath = "https://www.noraneko.co.jp/outlookonsendaddinsample/dialog.html";
+let mailItem;
+
+Office.onReady().then(function () {
+  mailItem = Office.context.mailbox.item;
+});
+
+function messageOnSent(event) {
+  const thisEvent = event;
+
+  if (Office.context.mailbox.diagnostics.hostName !== "OutlookWebApp") {
+    thisEvent.completed({
+      allowEvent: true,
+    });
+    return;
+  }
+  if (mailboxItem.itemType !== Office.MailboxEnums.ItemType.Message) {
+    thisEvent.completed({
+      allowEvent: true,
+    });
+    return;
+  }
+
+  Office.context.ui.displayDialogAsync(dialogPath, { height: 50, width: 50, promptBeforeOpen: false, displayInIframe: true }, function (asyncResult) {
+    dialog = asyncResult.value;
+    dialog.addEventHandler(Office.EventType.DialogMessageReceived, function (arg) {
+      dialog.close();
+      if (arg.message === "send") {
+        thisEvent.completed({
+          allowEvent: true,
+        });
+      } else {
+        thisEvent.completed({
+          allowEvent: false,
+        });
+      }
+    });
+    dialog.addEventHandler(Office.EventType.DialogEventReceived, function (arg) {
+      if (arg.error >= 12002) {
+        thisEvent.completed({
+          allowEvent: false,
+        });
+      }
+    });
+  });
+}
